b8501e86b5f249da95e878d7c0576040215ba579
cleverson <cleverson461@gmail.com>
2024-01-25 23:32:17 -0300

    Adicionado os arquivos

--- menu.js
+++ menu.js
@@ -13,3 +13,20 @@ menuMobile.addEventListener("click", () => {
 overlay.addEventListener("click", () => {
   menuMobile.classList.remove("abrir-menu")
 })
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
